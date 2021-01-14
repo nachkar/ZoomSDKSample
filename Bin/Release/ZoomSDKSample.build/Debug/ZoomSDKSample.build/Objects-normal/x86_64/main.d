5f28f92d5c9c597b1b216f92eb4c687e80970bc2
noelachkar <noelachkar@hotmail.com>
2021-01-14 18:58:39 +0200

    New commit

--- Bin/Release/ZoomSDKSample.build/Debug/ZoomSDKSample.build/Objects-normal/x86_64/main.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/nachkar/Downloads/zoom-sdk-macos-5.4.54528.1230/ZoomSDKSample/ZoomSDKSample/main.m \
-  /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX11.1.sdk/usr/include/mach-o/compact_unwind_encoding.modulemap \
-  /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX11.1.sdk/usr/include/mach-o/dyld.modulemap
